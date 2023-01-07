56fc8f747d901a13ef91f77f77b35f711ccd37b1
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2023-01-07 09:23:36 +0100

    Update test_translator.py

--- final_project/test_translator.py
+++ final_project/test_translator.py
@@ -6,11 +6,13 @@ class TestTranslator(unittest.TestCase):
         data = 'drink'
         result = translator.english_to_french(data)
         self.assertEqual(result, 'Boisson')
+        self.assertNotEqual(result, 'drink')
 
     def test_french_to_english(self):
         data = 'boire'
         result = translator.french_to_english(data)
         self.assertEqual(result, 'Drink')
+        self.assertNotEqual(result, 'boire')
 
 if __name__ == '__main__':
     unittest.main()
