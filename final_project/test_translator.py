46533ba3f449f1b52a047fce882ba81ea58d0d30
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2023-01-07 09:19:32 +0100

    Update test_translator.py

--- final_project/test_translator.py
+++ final_project/test_translator.py
@@ -1,4 +1,16 @@
-target = __import__("translator.py")
-translate_english_to_french = target.english_to_french
-translate_french_to_english = target.french_to_english
+import unittest
+import translator
 
+class TestTranslator(unittest.TestCase):
+    def test_english_to_french(self):
+        data = 'drink'
+        result = translator.english_to_french(data)
+        self.assertEqual(result, 'Boisson')
+
+    def test_french_to_english(self):
+        data = 'boire'
+        result = translator.french_to_english(data)
+        self.assertEqual(result, 'Drink')
+
+if __name__ == '__main__':
+    unittest.main()
