381aa275d0019e01863e23a9f24b6263238582e9
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2023-01-07 08:55:38 +0100

    Create translator.py

---
+++ final_project/translator.py
@@ -0,0 +1,10 @@
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('9zz14Prg2NuRnvaKKfsS7KQhBWZjotwru2M9ZJ8Dv2fJ')
+language_translator = LanguageTranslatorV3(
+    version='{version}',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/87da8875-8bc5-4728-b1bd-d3dc7292787d')
